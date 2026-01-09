6a6a1e55f8776404fbf77382432b23f439c341f2
Meykaye <Meykaye.socials@gmail.com>
2026-01-09 12:42:28 +0530

    Programming Elements Level 1 and Level 2 added

---
+++ src/ProgrammingElements/Level1/UniversityFeeInput.java
@@ -0,0 +1,19 @@
+package ProgrammingElements.Level1;
+import java.util.Scanner;
+
+public class UniversityFeeInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double fee = input.nextDouble();
+        double discountPercent = input.nextDouble();
+
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+
+        input.close();
+    }
+}
+
