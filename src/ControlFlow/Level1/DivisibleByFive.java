e467e7382ae30070fde1ca6c329e2788b99403f6
Meykaye <Meykaye.socials@gmail.com>
2026-01-09 14:16:15 +0530

    ControlFlow Level 1 files added

---
+++ src/ControlFlow/Level1/DivisibleByFive.java
@@ -0,0 +1,12 @@
+package ControlFlow.Level1;
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(num % 5 == 0 ? num +" is Divisible by 5" : num + " is not Divisible by 5");
+        sc.close();
+    }
+}
