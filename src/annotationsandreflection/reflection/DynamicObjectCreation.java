3d7349b01645a66acaa191d88e7fa83f9972140e
Meykaye <Meykaye.socials@gmail.com>
2026-02-21 11:24:57 +0530

    added annotations and reflection module

---
+++ src/annotationsandreflection/reflection/DynamicObjectCreation.java
@@ -0,0 +1,15 @@
+package annotationsandreflection.reflection;
+
+class Student {
+    public Student() {
+        System.out.println("Student object created");
+    }
+}
+
+public class DynamicObjectCreation {
+    public static void main(String[] args) throws Exception {
+
+        Class<?> cls = Class.forName(Student.class.getName());
+        Object obj = cls.getDeclaredConstructor().newInstance();
+    }
+}
